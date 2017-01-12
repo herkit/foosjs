21ed4fe4632073e66f1735bdf0a967b419c858d0
Henrik Grotle <henrik.grotle@linkmobility.com>
2017-01-12 15:33:30 +0100

    docker + compose

--- src/index.js
+++ src/index.js
@@ -145,3 +145,14 @@ var calculateTable = function(events) {
 }
 
 importEvents(calculateTable);
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send(players);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
