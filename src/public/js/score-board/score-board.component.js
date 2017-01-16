1553dfac9643dc641720a382cbe77e7541c4192b
Henrik Grotle <henrik.grotle@arasoft.no>
2017-01-17 00:49:56 +0100

    scoreboard template file

--- src/public/js/score-board/score-board.component.js
+++ src/public/js/score-board/score-board.component.js
@@ -3,13 +3,7 @@
 angular.
   module('foosjsApp').
   component("scoreBoard", {
-    template: 
-      '<ul>' +
-        '<li ng-repeat="player in $ctrl.scoreboard">' +
-          '<span>{{player.name}}</span>' +
-          '<p>{{player.rank}}</p>' +
-        '</li>' +
-      '</ul>',
+    templateUrl: 'js/score-board/score-board.template.html',
     controller: function ScoreBoardController($http) {
       var self = this;
       $http.get('/table').then(function(response) {
