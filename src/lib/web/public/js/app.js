e2df2b0e8a06409b47e0c9637615d9bd8e94bfe8
Henrik Grotle <henrik.grotle@arasoft.no>
2017-01-20 23:45:56 +0100

    fab that opens new event dialog

--- src/lib/web/public/js/app.js
+++ src/lib/web/public/js/app.js
@@ -1,3 +1,31 @@
 'use strict';
 
-angular.module('foosjsApp', ['ngMaterial']);
+angular.module('foosjsApp', ['ngMaterial'])
+
+.controller('FoosJsController', function FoosJsController($scope, $mdDialog) {
+  $scope.newEvent = function(ev) {
+    console.log("open dialog");
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'js/dialogs/new-event/new-event.template.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clicOutsideToClose:true,
+      fullscreen:true
+    })
+  }
+
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }  
+});
