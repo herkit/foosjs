6ec40d51e5c8bd161666ea0d46432e6599f22ce6
Henrik Grotle <henrik.grotle@arasoft.no>
2017-01-21 12:24:04 +0100

    scoreboard is using graph

--- src/lib/web/public/js/score-board/score-board.component.js
+++ src/lib/web/public/js/score-board/score-board.component.js
@@ -6,8 +6,23 @@ angular.
     templateUrl: 'js/score-board/score-board.template.html',
     controller: function ScoreBoardController($http) {
       var self = this;
-      $http.get('/table').then(function(response) {
-        self.scoreboard = response.data;
+      var query = encodeURIComponent(`{
+        lastSnapshot { 
+          _id, 
+          time, 
+          players {
+            player {_id, name, avatar}
+            rank
+            gamesPlayed
+            singlesWon
+            singlesLost
+            doublesWon
+            doublesLost
+          }
+        }
+      }`);
+      $http.get('/graph?query=' + query).then(function(response) {
+        self.scoreboard = response.data.data.lastSnapshot;
       });
       self.gotoPlayer = function(playerId) {
         console.log("Loading player", playerId);
