3fe027edb3db4908d0844bcde64e42a639bd2dff
Henrik Grotle <henrik.grotle@arasoft.no>
2017-01-22 21:47:16 +0100

    specifying event types

--- src/lib/graph/schema.js
+++ src/lib/graph/schema.js
@@ -19,12 +19,34 @@ var schema = buildSchema(`
     history: [PlayerState]
   }
 
-  type Event {
+  interface Event {
     _id: ID!
     time: String!
     seqNo: Int!
     type: String!
     what: String
+  }
+
+  type DoubleMatchEvent implements Event {
+    _id: ID!
+    time: String!
+    seqNo: Int!
+    type: String!
+    what: String
+    winner_1: Player!
+    winner_2: Player!
+    loser_1: Player!
+    loser_2: Player!   
+  }
+
+  type SingleMatchEvent implements Event {
+    _id: ID!
+    time: String!
+    seqNo: Int!
+    type: String!
+    what: String
+    winner_1: Player!
+    loser_1: Player!
   }
 
   type Snapshot {
