c0846be39ad061c8754d5e58d69b0cbb4c433a83
Henrik Grotle <henrik.grotle@arasoft.no>
2017-01-22 13:55:09 +0100

    websocket service

--- src/lib/web/public/js/services/api.service.js
+++ src/lib/web/public/js/services/api.service.js
@@ -31,3 +31,35 @@ angular.
   }).
   value('version', '0.1');
 
+angular.
+  module('foosjsApp').
+  factory('foosWss', function($rootScope) {
+    var socket = io.connect();
+    return {
+      on: function (eventName, callback) {
+        socket.on(eventName, function () {  
+          var args = arguments;
+          $rootScope.$apply(function () {
+            callback.apply(socket, args);
+          });
+        });
+      },
+      emit: function (eventName, data, callback) {
+        socket.emit(eventName, data, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            if (callback) {
+              callback.apply(socket, args);
+            }
+          });
+        })
+      }
+    };
+  })
+
+angular.
+  module('foosjsApp').
+  factory('foosScoreboard', function($rootScope, $http) {
+
+  });
+
