83000fb1a7b586bcd44b521adf583cfbc3957f81
Henrik Grotle <henrik.grotle@arasoft.no>
2017-01-22 23:41:39 +0100

    fancy event view

--- src/lib/web/public/js/last-events/last-events.component.js
+++ src/lib/web/public/js/last-events/last-events.component.js
@@ -10,10 +10,29 @@ angular.
     },
     templateUrl: 'js/last-events/last-events.template.html',
     controller: function LastEventsController($http) {
+      var query = encodeURIComponent(`{
+        events(first:5) {
+          _id 
+          time
+          type
+          seqNo
+          ... on DoubleMatchEvent { 
+            winner_1 { _id name avatar }
+            winner_2 { _id name avatar }
+            loser_1 { _id name avatar }
+            loser_2 { _id name avatar }
+          }
+          ... on SingleMatchEvent {
+            winner_1 { _id name avatar }
+            loser_1 { _id name avatar }
+          }
+        }
+      }`);
+
       var self = this;
 
-      $http.get('/events/last/' + self.count).then(function(response) {
-        self.events = response.data;
+      $http.get('/graph?query=' + query).then(function(response) {
+        self.events = response.data.data.events;
       });
 
       self.events = [];
